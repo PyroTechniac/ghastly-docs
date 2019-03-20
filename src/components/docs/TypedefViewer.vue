b5b8ac6143ea05b537a7927f2f0abfc98f5a32f0
PyroTechniac <39341355+PyroTechniac@users.noreply.github.com>
2019-03-20 19:00:50 -0400

    typedef viewer

---
+++ src/components/docs/TypedefViewer.vue
@@ -0,0 +1,81 @@
+<template>
+  <div v-if="typedef">
+    <div class="card">
+      <header class="card-header">
+        <nav class="card-header-title level is-marginless">
+          <div class="level-left">
+            <div class="level-item">
+              <span v-if="typedef.deprecated">
+                <span
+                  class="tag is-danger"
+                  title="This typedef is deprecated, and may be removed in a future version."
+                >Deprecated</span>&nbsp;
+              </span>
+            </div>
+            <span class="level-item is-size-3 is-marginless">{{ typedef.name }}&nbsp;</span>
+          </div>
+        </nav>
+        <source-button :meta="typedef.meta" :docs="docs" class="card-header-icon"/>
+      </header>
+      <div
+        v-if="typedef.description || (typedef.props && typedef.props.length)"
+        class="card-content"
+      >
+        <p v-if="typedef.description" class="subtitle" v-html="description"/>
+        <div v-if="typedef.props && typedef.props.length">
+          <strong>Properties:</strong>
+          <param-table :params="typedef.props" :docs="docs"/>
+        </div>
+      </div>
+      <footer class="card-footer">
+        <p class="card-footer-item">
+          <span>
+            <strong>Type:</strong>
+            <span v-for="(type, index) of typedef.type" :key="index">
+              <types :names="type" :nullable="typedef.nullable" :docs="docs"/>
+              <span v-if="index < typedef.type.length - 1">|</span>
+            </span>
+          </span>
+        </p>
+      </footer>
+    </div>
+    <br>
+  </div>
+  <unknown-page v-else/>
+</template>
+
+<script>
+import Vue from "vue";
+import { convertLinks } from "../../util";
+import Types from "./Types.vue";
+import ParamTable from "./class-viewer/ParamTable.vue";
+import SourceButton from "./SourceButton.vue";
+export default {
+  name: "TypedefViewer",
+  components: {
+    Types,
+    ParamTable,
+    SourceButton
+  },
+  props: ["docs"],
+  data() {
+    return {
+      typedef: this.docs.typedefs.find(
+        typ => typ.name === this.$route.params.typedef
+      )
+    };
+  },
+  computed: {
+    description() {
+      return Vue.filter("marked")(
+        convertLinks(
+          this.typedef.description,
+          this.docs,
+          this.$router,
+          this.$route
+        )
+      );
+    }
+  }
+};
+</script>
