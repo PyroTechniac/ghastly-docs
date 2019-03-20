d6ac8766e75117f9b46799cfc59bcf5e09e06fc1
Owen Bellish <shadowguy224@live.com>
2019-03-20 10:22:28 -0400

    base commit

---
+++ poi.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        fileNames: {
+            js: 'js/[name].js',
+            css: 'css/[name].css'
+        },
+        html: { template: './index.html' }
+    },
+    plugins: [
+        { resolve: '@poi/plugin-eslint' }
+    ]
+};
