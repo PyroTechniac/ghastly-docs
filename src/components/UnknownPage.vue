19dbb4f1c87075964dc8ec755c12347c668a6237
PyroTechniac <39341355+PyroTechniac@users.noreply.github.com>
2019-03-20 17:53:40 -0400

    finish unknown page

---
+++ src/components/UnknownPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="content">
+    <h1>Unknown {{ type || 'page' }}</h1>
+    <p>
+      This page doesn't seem to exist on the web.
+      <b-icon icon="frown" />
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'UnknownPage',
+    props: ['type']
+};
+</script>
