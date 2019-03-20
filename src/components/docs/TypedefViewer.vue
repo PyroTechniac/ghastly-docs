ecee9f8c747cddf60d470bcfd458e87d9ac4d813
PyroTechniac <39341355+PyroTechniac@users.noreply.github.com>
2019-03-20 19:01:13 -0400

    linting

--- src/components/docs/TypedefViewer.vue
+++ src/components/docs/TypedefViewer.vue
@@ -15,24 +15,42 @@
             <span class="level-item is-size-3 is-marginless">{{ typedef.name }}&nbsp;</span>
           </div>
         </nav>
-        <source-button :meta="typedef.meta" :docs="docs" class="card-header-icon"/>
+        <source-button
+          :meta="typedef.meta"
+          :docs="docs"
+          class="card-header-icon"
+        />
       </header>
       <div
         v-if="typedef.description || (typedef.props && typedef.props.length)"
         class="card-content"
       >
-        <p v-if="typedef.description" class="subtitle" v-html="description"/>
+        <p
+          v-if="typedef.description"
+          class="subtitle"
+          v-html="description"
+        />
         <div v-if="typedef.props && typedef.props.length">
           <strong>Properties:</strong>
-          <param-table :params="typedef.props" :docs="docs"/>
+          <param-table
+            :params="typedef.props"
+            :docs="docs"
+          />
         </div>
       </div>
       <footer class="card-footer">
         <p class="card-footer-item">
           <span>
             <strong>Type:</strong>
-            <span v-for="(type, index) of typedef.type" :key="index">
-              <types :names="type" :nullable="typedef.nullable" :docs="docs"/>
+            <span
+              v-for="(type, index) of typedef.type"
+              :key="index"
+            >
+              <types
+                :names="type"
+                :nullable="typedef.nullable"
+                :docs="docs"
+              />
               <span v-if="index < typedef.type.length - 1">|</span>
             </span>
           </span>
@@ -41,7 +59,7 @@
     </div>
     <br>
   </div>
-  <unknown-page v-else/>
+  <unknown-page v-else />
 </template>
 
 <script>
