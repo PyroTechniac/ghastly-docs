ddf8546fedbde7e43273b50f52ebf8f6c2c36845
Owen Bellish <shadowguy224@live.com>
2019-03-20 14:27:44 -0400

    adding base components

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,60 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <div class="content has-text-centered">
+        <p>
+          <strong>Placeholder</strong>
+        </p>
+        <div class="columns">
+          <div class="column is-two-fifths is-hidden-tablet"/>
+          <div class="column">
+            <a href="https://www.npmjs.com/package/discord-entangled" class="button is-danger">
+              <span>Published using:</span>
+              <span class="icon">
+                <i class="fab fa-npm"/>
+              </span>
+            </a>
+          </div>
+          <div class="column">
+            <a href="https://www.patreon.com/PyroTechniac" class="button is-patreon">
+              <span>Support me:</span>
+              <span class="icon">
+                <i class="fab fa-patreon"/>
+              </span>
+            </a>
+          </div>
+          <div class="column">
+            <a class="button is-dark" @click="toggleDark()">
+              <span>{{dark ? "Light" : "Dark"}} Mode</span>
+              <span class="icon">
+                <i :class="dark ? 'fas fa-sun' : 'fas fa-moon'"/>
+              </span>
+            </a>
+          </div>
+          <div class="column is-two-fifths is-hidden-tablet"/>
+        </div>
+        <stats/>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+import Stats from './Stats.vue';
+
+export default {
+    name: 'AppFooter',
+    components: {Stats},
+    computed: {
+        dark() {
+            return this.$parent.dark;
+        }
+    },
+    methods: {
+        toggleDark() {
+            this.$parent.dark = !this.$parent.dark;
+            localStorage.setItem('dark', String(this.$parent.dark));
+        }
+    }
+};
+</script>
---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="notification is-info is-marginless is-radiusless is-paddingless">
+    <div class="container">
+      <nav class="level is-mobile">
+        <!-- Left side -->
+        <div class="level-left">
+          <div class="level-item">
+            <router-link class="navbar-item button is-info title is-5 is-marginless" to="/"></router-link>
+          </div>
+        </div>
+
+        <!-- Right side -->
+        <div class="level-right">
+          <div class="level-item">
+            <router-link class="navbar-item button is-info is-5 is-marginless" to="/docs">
+              <span class="icon">
+                <b-icon icon="book"/>
+              </span>
+              <span class="is-hidden-mobile">Docs</span>
+            </router-link>
+          </div>
+          <div class="level-item">
+            <a class="navbar-item button is-info is-5 is-marginless" @click="invite()">
+              <span class="icon">
+                <i class="fab fa-discord"/>
+              </span>
+              <span class="is-hidden-mobile">Discord</span>
+            </a>
+          </div>
+          <div class="level-item">
+            <a
+              :href="`https://github.com/PyroTechniac/${this.$route.params.source || 'discord-entangled'}/tree/${this.$route.params.tag || 'master'}`"
+              class="navbar-item button is-info is-5 is-marginless"
+            >
+              <span class="icon">
+                <i class="fab fa-github"/>
+              </span>
+              <span class="is-hidden-mobile">GitHub</span>
+            </a>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'AppNavbar',
+    computed: {
+        invite() {
+            return this.$parent.invite;
+        }
+    }
+}
+</script>
